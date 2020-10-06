f03138db270194e945ce6b373915f964dba562fd
roshni-patel <rpatel01@wesleyan.edu>
2020-10-06 17:41:20 -0500

    Edited channel.rb list_all and details methods

--- lib/channel.rb
+++ lib/channel.rb
@@ -4,20 +4,25 @@ require 'table_print'
 class Channel < Recipient
   attr_reader :topic, :member_count
 
-  def initialize(slack_id, name, topic, member_count)
+  def initialize(slack_id:, name:, topic:, member_count:)
     super(slack_id, name)
     @topic = topic
     @member_count = member_count
   end
 
   def details
-    return tp :slack_id, :name, :topic, :member_count
+    tp self.all
   end
 
   def self.list_all
-    response = HTTParty.get("conversations.list")
-    all_channels = response["channels"].map do |channel|
-      Channel.new(channel["id"], channel["name"], channel["topic"]["value"], channel["num_members"])
+    url = "https://slack.com/api/conversations.list"
+    param = {token: ENV[SLACK_TOKEN]}
+    raw_channels = Channel.get(url, param)['channels']
+    all_channels = raw_channels.map do |channel|
+      Channel.new(slack_id: channel["id"],
+                  name: channel["name"],
+                  topic: channel["topic"]["value"],
+                  member_count: channel["num_members"])
     end
     return all_channels
   end
