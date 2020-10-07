53c5c909258df66c9d4090e01727d86355380828
roshni-patel <rpatel01@wesleyan.edu>
2020-10-06 22:45:15 -0500

    Added some more assertions to channel.rb test

--- test/channel_test.rb
+++ test/channel_test.rb
@@ -8,22 +8,39 @@ describe "Channel" do
   describe "Initialize method" do
     it "it creates an instance of channel" do
       expect(@channel).must_be_kind_of Channel
+      expect(@channel.slack_id).must_equal "C01BKP7MWNB"
+      expect(@channel.name).must_equal "random"
+      expect(@channel.topic).must_equal ""
+      expect(@channel.member_count).must_equal 2
     end
   end
 
   describe "Details method" do
     it "lists the details for a channel" do
+      # Arrange and Act
+      channel_details = @channel.details
+
+      # Assert
       expect(@channel.details).must_be_kind_of Hash
+      expect(channel_details[:SLACK_ID]).must_equal @channel.slack_id
+      expect(channel_details[:NAME]).must_equal @channel.name
+      expect(channel_details[:TOPIC]).must_equal @channel.topic
+      expect(channel_details[:MEMBER_COUNT]).must_equal @channel.member_count
     end
   end
 
   describe "List all method" do
     it "returns all channels in the workspace" do
       VCR.use_cassette("list_all") do
+        # Arrange
         CHANNELS_URL = "https://slack.com/api/conversations.list"
         SLACK_TOKEN = ENV["SLACK_TOKEN"]
         response = HTTParty.get(CHANNELS_URL, query: {token: SLACK_TOKEN })["channels"]
+
+        # Act
         channels = Channel.list_all
+
+        # Assert
         expect(Channel.list_all.length).must_equal response.length
         response.length.times do |i|
           expect(response[i]["id"]).must_equal channels[i].slack_id
