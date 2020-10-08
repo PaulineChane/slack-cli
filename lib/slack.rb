d2610b5d0a9db1857cc8ea140d1a915ce20d4616
roshni-patel <rpatel01@wesleyan.edu>
2020-10-07 22:51:09 -0500

    Updated slack CLI with clear selection method

--- lib/slack.rb
+++ lib/slack.rb
@@ -7,18 +7,18 @@ def main
   Dotenv.load
   puts "Welcome to the Ada Slack CLI!"
   workspace = Workspace.new
+  selected_recipient = nil
 
   puts
   puts "This workspace has #{workspace.users.length} users and #{workspace.channels.length} channels"
   puts
-  puts "Choose from the following \n 1. list channels \n 2. list users \n 3. select user \n 4. select channel \n 5. details \n 6. send message \n 7. quit"
+  puts "Choose from the following \n 1. list channels \n 2. list users \n 3. select user \n 4. select channel \n 5. details \n 6. send message \n 7. clear selection \n 8. quit \nSelected Recipient(NONE if blank): #{selected_recipient}"
   puts
 
   user_input = ""
   until user_input == "quit"
     puts "What would you like to do?"
     user_input = gets.strip.downcase
-    selected_recipient = nil
 
     case user_input
     when "list channels"
@@ -27,46 +27,46 @@ def main
       tp workspace.users, :slack_id, :name, :real_name, :status_text, :status_emoji
     when "select user"
       puts "Which user would you like to select?"
-      selected_recipient = gets.chomp
+      selected_recipient = gets.strip
       workspace.select_user(selected_recipient)
       if workspace.selected.nil?
         puts "No user by the name #{selected_recipient} can be found. Please try again."
       end
     when "select channel"
       puts "Which channel would you like to select?"
-      selected_recipient = gets.chomp
+      selected_recipient = gets.strip
       workspace.select_channel(selected_recipient)
       if workspace.selected.nil?
         puts "No channel by the name #{selected_recipient} can be found. Please try again."
       end
     when "details"
-      puts selected_recipient
       if workspace.selected.nil?
         puts "Please select a channel or user before asking for details."
       end
       workspace.show_details
-      selected_recipient = nil
     when "send message"
-      puts selected_recipient
       if workspace.selected.nil?
         puts "Please select a channel or user before sending a message."
       else
         puts "What is the message you would like to send?"
-        message = gets.chomp
+        message = gets.strip
         begin
           workspace.send_message(message)
         rescue SlackApiError => error
           puts error.message
         end
-        message = nil
       end
+    when "clear selection"
+      workspace.clear_selection
+      selected_recipient = nil
     when "quit"
       user_input = "quit"
+      break
     else
       puts "That's not a valid option. Please try again."
     end
 
-    puts "Choose from the following \n 1. list channels \n 2. list users \n 3. select user \n 4. select channel \n 5. details \n 6. send message \n 7. quit"
+    puts "Choose from the following \n 1. list channels \n 2. list users \n 3. select user \n 4. select channel \n 5. details \n 6. send message \n 7. clear selection \n 8. quit \nSelected Recipient(NONE if blank): #{selected_recipient}"
   end
 
   puts "Thank you for using the Ada Slack CLI!"
